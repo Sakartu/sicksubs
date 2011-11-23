d4c8dfb6b06aacf1a40a3627bf4126cf711d74d5
Peter Wagenaar <sakartu@gmail.com>
2011-11-23 23:47:28 +0100

    oops, final_loc has index 2

--- ep.py
+++ ep.py
@@ -7,8 +7,9 @@ class Ep(object):
 
     def from_row(self, row):
         self.job_name = row[0]
-        self.final_loc = row[1]
+        self.final_loc = row[2]
         self.tvdbid = row[3]
         self.sid = db.get_sid(self.conn, self.tvdbid)
         (self.season, self.ep) = nameparser.get_ep_details(self.job_name)
         self.sub = None
+        return self
