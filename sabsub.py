2be03f7772337fcf852c32ecaf43f3805ace247e
Peter Wagenaar <sakartu@gmail.com>
2011-11-23 23:47:48 +0100

    general overhaul to use sqlite database

--- sabsub.py
+++ sabsub.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+import db
 import os
 import sys
 import pickle
+import sqlite3
 import urllib2
 import bierdopje
 import nameparser
@@ -11,7 +13,7 @@ import nameparser
 # quite sane 
 
 # location where sabsub should store the queue file
-QUEUE_FILE = u'/tmp/sabsub_queue' 
+DATABASE_FILE = u'~/.sabsub/sabsub.db' 
 
 # the language of the downloaded subs, can be nl or en
 SUB_LANG = 'en'
@@ -19,17 +21,7 @@ SUB_LANG = 'en'
 #***********************************</CONFIG>***********************************
 
 
-class Queueitem(object):
-    def __init__(self, job_name, interm_loc, final_loc=None, tvdbid=None):
-        self.job_name = job_name
-        self.interm_loc = interm_loc
-        self.final_loc = final_loc
-        self.tvdbid = tvdbid
- 
-    def __repr__(self):
-        return str(self.tvdbid) + ":" + str(self.job_name)
-
-def sabnzbd_run():
+def sabnzbd_run(conn):
     '''
     This function will be called when the script is executed by sabnzbd+. This
     will add a show job to the queue, so that it can be downloaded from
@@ -49,12 +41,9 @@ def sabnzbd_run():
 
     interm_loc = sys.argv[1]
     job_name = sys.argv[3]
+    db.add_ep(conn, job_name, interm_loc)
 
-    jobs = read_queue()
-    jobs.add(Queueitem(job_name, interm_loc))
-    write_queue(jobs)
-
-def sickbeard_run():
+def sickbeard_run(conn):
     '''
     This function will be called when the script is executed by sickbeard. This
     will add a final_location to the correct item in the queue, to make sure the
@@ -71,42 +60,34 @@ def sickbeard_run():
     interm_loc = sys.argv[2]
     tvdbid = sys.argv[3]
 
-    jobs = read_queue()
-    for job in jobs:
-        if isinstance(job, Queueitem) and job.interm_loc == interm_loc:
-            job.final_loc = final_loc
-            job.tvdbid = tvdbid
-            write_queue(jobs)
-            return
+    db.update_ep(conn, interm_loc, final_loc, tvdbid)
 
-def cron_run(jobs=None):
+def cron_run(conn):
     '''
     This function will be called when the script is executed by cron. This will
     read the jobs and try to find sub downloads for each of them
     '''
-    if not jobs:
-        jobs = read_queue()
+    # get all eps
+    all_eps = db.get_all_eps(conn)
+    
+    to_download = []
+    for ep in all_eps:
+        if ep.sid and ep.season and ep.ep:
+            sublinks = bierdopje.get_subs(ep.sid, SUB_LANG, ep.season, ep.ep)
+            sub = nameparser.find_link(ep.job_name, sublinks)
+            if sub:
+                ep.sub = sub
+                to_download.append(ep)
 
-    to_download = set()
-    for job in jobs:
-        sid = bierdopje.get_show_id(job.tvdbid)
-        (season, ep) = nameparser.get_ep_details(job.job_name)
-        if sid and season and ep:
-            sublinks = bierdopje.get_subs(sid, SUB_LANG, season, ep)
-            job.sub = nameparser.find_link(job, sublinks)
-            if job.sub:
-                to_download.add(job)
-
-    for job in to_download:
-        download(job.sub, job.final_loc)
-    write_queue(set(jobs) - to_download)
+    download(to_download)
+    db.remove_downloaded(conn, to_download)
 
 def read_queue():
     '''
     This helper function opens the queue file and checks whether the type is
     correct
     '''
-    with open(QUEUE_FILE, 'rb') as f:
+    with open(DATABASE_FILE, 'rb') as f:
         jobs = pickle.load(f)
 
     if type(jobs) != type(set()):
@@ -118,31 +99,48 @@ def write_queue(jobs):
     '''
     This helper function opens the queue file and writes a new job queue to it
     '''
-    with open(QUEUE_FILE, 'w+') as f:
+    with open(DATABASE_FILE, 'w+') as f:
         pickle.dump(jobs, f)
 
-def download(url, loc):
+def download(to_download):
     '''
     This helper method downloads a sub to a filed named as the episode, but with
     a subtitle extension
     '''
-    baseloc = os.path.splitext(os.path.expanduser(loc))[0]
-    resp = urllib2.urlopen(url)
+    for ep in to_download:
+        baseloc = os.path.splitext(os.path.expanduser(ep.final_loc))[0]
+        resp = urllib2.urlopen(ep.sub)
 
-    if 'content-disposition' in resp.info().dict:
-        subext = os.path.splitext(resp.info().dict['content-disposition'])[1]
-    else:
-        subext = '.srt'
-    with open(baseloc + subext, 'w+') as sub:
-        sub.write(resp.read())
+        if 'content-disposition' in resp.info().dict:
+            subext = os.path.splitext(resp.info().dict['content-disposition'])[1]
+        else:
+            subext = '.srt'
+        content = resp.read()
+        with open(os.path.join(baseloc + subext), 'w+') as sub:
+            sub.write(content)
+
+def update_tvdbids(sids, tvdbid):
+    if tvdbid not in sids:
+        sid = bierdopje.get_show_id(tvdbid)
+        sids[tvdbid] = sid
+    return sids
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 8:
-        sabnzbd_run()
-    elif len(sys.argv) == 7:
-        sickbeard_run()
+    if len(sys.argv) == 2:
+        db_path = sys.argv[2]
     else:
-        t = Queueitem('Terra.Nova.S01E06.HDTV.XviD-LOL', 'bla', '/media/media/Series/Terra Nova/Season 01/Terra.Nova.S01E06.Nightfall.avi', tvdbid='164091')
-        #t = Queueitem('Terra Nova', 'bla', 'bla')
-        cron_run([t])
+        db_path = DATABASE_FILE
+
+    if not os.path.exists(db_path):
+        conn = db.initialize(db_path)
+    else:
+        conn = sqlite3.connect(db_path)
+
+    if len(sys.argv) == 8:
+        sabnzbd_run(conn)
+    elif len(sys.argv) == 7:
+        sickbeard_run(conn)
+    else:
+        cron_run(conn)
 
