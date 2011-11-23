9e023630633a9217162d36fb473be8ad43f6d1c0
Peter Wagenaar <sakartu@gmail.com>
2011-11-23 21:22:48 +0100

    added from_row func

--- ep.py
+++ ep.py
@@ -2,10 +2,13 @@ import db
 import nameparser
 
 class Ep(object):
-    def __init__(self, row):
+    def __init__(self, conn):
+        self.conn = conn
+
+    def from_row(self, row):
         self.job_name = row[0]
         self.final_loc = row[1]
         self.tvdbid = row[3]
-        self.sid = db.get_sid(self.tvdbid)
+        self.sid = db.get_sid(self.conn, self.tvdbid)
         (self.season, self.ep) = nameparser.get_ep_details(self.job_name)
         self.sub = None
