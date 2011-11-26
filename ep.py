1afa364809b8e50d947ec454bb05b7459fe88c9f
Peter Wagenaar <sakartu@gmail.com>
2011-11-26 13:27:37 +0100

    added interm_loc, changed row indexes, added job_name through parser

--- ep.py
+++ ep.py
@@ -6,9 +6,10 @@ class Ep(object):
         self.conn = conn
 
     def from_row(self, row):
-        self.job_name = row[0]
-        self.final_loc = row[2]
-        self.tvdbid = row[3]
+        self.interm_loc = row[0]
+        self.final_loc = row[1]
+        self.tvdbid = row[2]
+        self.job_name = nameparser.get_job_name(self.interm_loc)
         self.sid = db.get_sid(self.conn, self.tvdbid)
         (self.season, self.ep) = nameparser.get_ep_details(self.job_name)
         self.sub = None
