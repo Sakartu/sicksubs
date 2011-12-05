72144c65c4a0dd987b1c2eb0ecad3b147b9f2077
Peter Wagenaar <sakartu@gmail.com>
2011-12-05 15:06:05 +0100

    match based on releasegroup

--- nameparser.py
+++ nameparser.py
@@ -2,7 +2,6 @@ import re
 import os
 rexes = ['S(\d\d)E(\d\d)', '(\d{1,2})x(\d{1,2})', '(\d)(\d\d)'] #in order
 
-
 def get_ep_details(line):
     for rex in rexes:
         c = re.compile(rex, re.I)
@@ -16,9 +15,22 @@ def get_ep_details(line):
 def find_link(name, sublinks):
     if not sublinks:
         return None
+    grp = get_release_group(name)
     for link in sublinks:
-        if name in link:
+        if grp in link:
             return link
+
+def get_release_group(name):
+    '''
+    Get the release group from a name. For instance, in the case of 
+    lost.girl.s02e11.hdtv.xvid-2hd.avi
+    returns '-2hd'
+    '''
+    name = os.path.splitext(name)[0]
+    if name:
+        return name[name.rfind('-'):]
+    else:
+        return name
 
 def get_job_name(loc):
     if '/' in loc:
