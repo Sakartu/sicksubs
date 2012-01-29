a09010aca1a6c99c848842d5a7c0097f41197d72
Peter Wagenaar <sakartu@gmail.com>
2012-01-29 18:08:55 +0100

    added a not_found test and a post_processing test

--- test.py
+++ test.py
@@ -4,20 +4,13 @@ import db
 import sicksubs
 import sys
 import os
+import glob
 import shutil
 
 workdir = '/tmp/sicksubs_test/'
 interm = os.path.join(workdir, 'interm')
 final = os.path.join(workdir, 'final')
 
-sick_argv = [os.path.join(os.path.abspath('.'), sys.argv[0]),
-        '/tmp/sicksubs_test/final/White.Collar.S02E04.By.the.Book.avi',
-        '/tmp/sicksubs_test/interm/White.Collar.S02E04.DVDRip.XviD-SAiNTS.avi',
-        '108611',
-        '2',
-        '5',
-        '03-09-2010'
-        ]
 
 class SickSubsTests(unittest.TestCase):
     def setUp(self):
@@ -30,11 +23,35 @@ class SickSubsTests(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(workdir)
 
-    def test_run(self):
-        sys.argv = sick_argv
+    def test_found(self):
+        # existing ep
+        sys.argv = [os.path.join(os.path.abspath('.'), sys.argv[0]),
+        '/tmp/sicksubs_test/final/White.Collar.S02E04.By.the.Book.avi',
+        '/tmp/sicksubs_test/interm/White.Collar.S02E04.DVDRip.XviD-SAiNTS.avi',
+        '108611',
+        '2',
+        '5',
+        '03-09-2010'
+        ]
+        # no need to run the cron run, the sickbeard run will do that for us
         sicksubs.sickbeard_run(self.conn)
-        sicksubs.cron_run(self.conn)
-        self.assertTrue(os.path.exists('/tmp/sicksubs_test/final/White.Collar.S02E04.By.the.Book.srt'))
+        self.assertTrue(os.path.exists(final + '/White.Collar.S02E04.By.the.Book.srt'))
+        self.assertTrue(os.path.exists(final + '/White.Collar.S02E04.By.the.Book.avi.works'))
+
+    def test_not_found(self):
+        # use a non existing ep
+        sys.argv = [os.path.join(os.path.abspath('.'), sys.argv[0]),
+        '/tmp/sicksubs_test/final/White.Collar.S02E40.By.the.Book.avi',
+        '/tmp/sicksubs_test/interm/White.Collar.S02E40.DVDRip.XviD-SAiNTS.avi',
+        '108611',
+        '2',
+        '5',
+        '03-09-2010'
+        ]
+        # no need to run the cron run, the sickbeard run will do that for us
+        sicksubs.sickbeard_run(self.conn)
+        self.assertEqual([], glob.glob(final + '/*.srt'))
+        self.assertEqual([], glob.glob(final + '/*.works'))
 
 if __name__ == '__main__':
     tests = unittest.main()
