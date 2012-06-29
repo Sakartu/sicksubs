525bb15b96fbbdefb719f531d4ed081f140bc166
Peter Wagenaar <sakartu@gmail.com>
2012-06-29 19:47:16 +0200

    add show quality filter and smart downloading

--- find_subs.py
+++ find_subs.py
@@ -12,68 +12,98 @@ import nameparser
 
 
 def main():
-    if sys.argv[1:]:
-        showname = ' '.join(sys.argv[1:])
-    else:
-        showname = raw_input(u'What show would you like subs for? ')
-    showinfos = bierdopje.find_shows_by_name(showname)
-
-    maxlen = max(map(lambda x: len(x[0]), showinfos))
-    showlist = 'id.   ' + 'Name'.ljust(maxlen) + '   Aired\n'
-    for i, (name, firstaired, sid, seasons) in enumerate(showinfos):
-        if not firstaired:
-            firstaired = '?'
-        showlist += '{id:02d}.   {name:{maxlen}s}   {firstaired}\n'.format(
-                id=i + 1,
-                maxlen=maxlen,
-                name=name,
-                firstaired=firstaired)
-
-    print showlist
-    answer = raw_input(u'Which show (id) would you like me to download subs '
-    'for? ')
     try:
-        show = showinfos[int(answer) - 1]
-        sid = show[2]
-    except:
-        print u'Invalid id chosen or server returned garbage!'
-        sys.exit(-1)
-    try:
-        s = raw_input(u'For which season would you like to download subs, '
-                '[{0}-{1}]? '.format(1, int(show[3])))
-    except:
-        print u'Invalid season provided!'
-        sys.exit(-1)
+        if sys.argv[1:]:
+            showname = ' '.join(sys.argv[1:])
+        else:
+            showname = raw_input(u'What show would you like subs for? ')
+        showinfos = bierdopje.find_shows_by_name(showname)
 
-    lang = raw_input(u'Which lang would you like to download subs for? '
-    '[en] ').strip()
-    if lang not in ('nl', 'en'):
-        lang = 'en'
+        maxlen = max(map(lambda x: len(x[0]), showinfos))
+        showlist = 'id.   ' + 'Name'.ljust(maxlen) + '   Aired\n'
+        for i, (name, firstaired, sid, seasons) in enumerate(showinfos):
+            if not firstaired:
+                firstaired = '?'
+            showlist += '{id:02d}.   {name:{maxlen}s}   {firstaired}\n'.format(
+                    id=i + 1,
+                    maxlen=maxlen,
+                    name=name,
+                    firstaired=firstaired)
 
-    all_subs = bierdopje.get_subs_by_season(sid, lang, s)
-    to_download = []
-    maxsub = max(all_subs)
-    e = 1
-    while e <= maxsub:
-        sublist = ''
-        for i, (name, link) in enumerate(all_subs[e]):
-            sublist += '{id:02d}.   {name}\n'.format(id=i + 1, name=name)
-        print sublist
+        print showlist
+        answer = raw_input(u'Which show (id) would you like me to download '
+        'subs for? ')
         try:
-            answer = int(raw_input(u'Which sub (id) would you like me to '
-            'download? '))
-            if answer < 1 or answer > (i + 1):
-                raise Exception
-            to_download.append(all_subs[e][answer - 1])
-        except KeyboardInterrupt:
-            print u'Aborting...'
-            sys.exit(-1)
+            show = showinfos[int(answer) - 1]
+            sid = show[2]
         except:
-            print u'Garbage or too large id given, continuing with next'
-        e += 1
-    print u'Downloading...'
-    for d in to_download:
-        download(d)
+            print u'Invalid id chosen or server returned garbage!'
+            sys.exit(-1)
+        try:
+            s = int(raw_input(u'For which season would you like to download '
+            'subs, [{0}-{1}]? '.format(1, int(show[3]))))
+            if s < 1 or s > int(show[3]):
+                raise Exception
+        except:
+            print u'Invalid season provided!'
+            sys.exit(-1)
+
+        lang = raw_input(u'Which lang would you like to download subs for '
+        '(en/nl)? [en] ').lower().strip()
+        if lang not in ('nl', 'en'):
+            lang = 'en'
+
+        qual = raw_input(u'Do you prefer HD rips? [yes] ').lower().strip()
+        hd = ('y' in qual or qual.strip() == '')
+
+        all_subs = bierdopje.get_subs_by_season(sid, lang, s)
+        to_download = []
+        maxsub = max(all_subs)
+        e = 1
+        while e <= maxsub:
+            try:
+                if e not in all_subs:
+                    print u'Could not find subs for ep number {0}!'.format(e)
+                    continue
+
+                possibles = nameparser.filter_qual(all_subs[e], hd)
+                if not possibles:
+                    print u'Could not find subs for ep number {0}!'.format(e)
+                    continue
+                if len(possibles) == 1:
+                    # only one choice, download immediately
+                    print (u'Only one choice for ep number {0}, will download '
+                    '{1}!').format(e, possibles[0][0])
+                    to_download.append(possibles[0])
+                    continue
+                print (u'Multiple choices for ep number {0}, please pick '
+                'one:').format(e)
+                for i, (name, link) in enumerate(possibles):
+                    print '{id:02d}.   {name}'.format(
+                            id=i + 1,
+                            name=name)
+                try:
+                    answer = int(raw_input(u'Which sub (id) would you like me '
+                    'to download? '))
+                    if answer < 1 or answer > (i + 1):
+                        raise Exception
+                    to_download.append(possibles[answer - 1])
+                except KeyboardInterrupt:
+                    raise KeyboardInterrupt
+                except:
+                    print u'Garbage or too large id given, continuing with '
+                    'next...'
+            finally:
+                e += 1
+        print u'Downloading...'
+        for d in to_download:
+            download(d)
+
+        print (u'Found subs for {num} out of {total} eps, have fun '
+        'watching!').format(num=len(to_download), total=maxsub)
+    except KeyboardInterrupt:
+        print u'Aborting...'
+        sys.exit(-1)
 
 
 def download(sub):
@@ -84,8 +114,14 @@ def download(sub):
     try:
         resp = urllib2.urlopen(link)
 
+        if 'content-disposition' in resp.info().dict:
+            filename = resp.info().dict['content-disposition']
+            subext = os.path.splitext(filename)[1]
+        else:
+            subext = '.srt'
+
         content = resp.read()
-        sub_path = './' + name
+        sub_path = './' + name + subext
         with open(sub_path, 'w+') as sub:
             sub.write(content)
             print "Successfully downloaded sub {0}".format(name)
